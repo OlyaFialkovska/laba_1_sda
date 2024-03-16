77192d8d4899b842f58acc54d181c224f036996a
Olya <106016782+OlyaFialkovska@users.noreply.github.com>
2024-03-16 11:23:51 +0200

    laba_sda_1_2

--- main.c
+++ main.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-    printf("hello world!\n");
-    return 0;
+    float x,y;
+    printf("x = ");
+    scanf("%f",&x);
+    if((x > -30) && (x <= -15) || (x > 3) && (x <= 20))//+
+    {
+        printf("undefined");
+    }
+    else//-
+    {
+        if ( (x <= 3) && (x > -15) )//+
+        {
+            y = (4*x*x+2);
+            printf("y = %f", y);
+        }
+        else//-
+        {
+            y = (3*x*x*x)/4-5;
+            printf("y = %f", y);
+        }
+        return 0;
+    }
 }
